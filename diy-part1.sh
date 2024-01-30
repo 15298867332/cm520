275af767eaa3d83b847f04e81730097bdab600d2
15298867332 <100286678+15298867332@users.noreply.github.com>
2024-01-30 11:00:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,13 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 ####################################################################
 rm -rf package/feeds/luci/luci-app-openclash
-./scripts/feeds update -a && ./scripts/feeds install -a
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+#########################################################################
 src-git small8 https://github.com/kenzok8/small-package
 ./scripts/feeds update -a && ./scripts/feeds install -a
 # xfsprogs
@@ -23,3 +29,11 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 sed -i 's/PKG_BUILD_DEPENDS:=golang/host/PKG_BUILD_DEPENDS:=golang/host upx/host/g' package/feeds/small8/speedtest-web/Makefile
 # Modify default IP
 sed -i 's/192.168.1./192.168.2./g' package/base-files/files/bin/config_generate
+# 修改本地时间格式
+sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+
+./scripts/feeds update -a && ./scripts/feeds install -a
